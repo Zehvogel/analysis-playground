e2c7456c19d76ebb2a84b9c88e40d6d196395063
Leonhard Reichenbach <leonhard.reichenbach@desy.de>
2021-11-05 15:48:44 +0100

    more root plots yay

--- plot_SLDs.C
+++ plot_SLDs.C
@@ -13,13 +13,33 @@
 // put ROOT includes here
 #include <TH2F.h>
 #include <TCanvas.h>
+#include <TVector3.h>
+#include <THStack.h>
 
 
 #include <numeric>
 
 using namespace lcio;
 
-void plot_SLDs(const char *fileName = "/afs/desy.de/group/flc/pool/reichenl/myMarlinProcessors/SLDLeptonSelector/test_out.slcio", const TString outname = "sld_out") {
+void binLogX(TH1 *h) 
+{
+    auto axis = h->GetXaxis();
+    int bins = axis->GetNbins();
+
+    auto from = axis->GetXmin();
+    auto to = axis->GetXmax();
+    auto width = (to - from) / bins;
+    auto new_bins = new Axis_t[bins + 1];
+
+    for (int i = 0; i <= bins; i++) {
+        new_bins[i] = TMath::Power(10, from + i * width);
+    }
+    axis->Set(bins, new_bins);
+    delete[] new_bins;
+}
+
+void plot_SLDs(const char *fileName = "/afs/desy.de/group/flc/pool/reichenl/myMarlinProcessors/SLDLeptonSelector/test_out.slcio", const TString outname = "sld_out")
+{
     const char *mcColName = "MCSLDLeptons";
     const char *relColname = "SLDMCRecoLink";
 
@@ -42,6 +62,41 @@ void plot_SLDs(const char *fileName = "/afs/desy.de/group/flc/pool/reichenl/myMa
     std::iota(ybins.begin(), ybins.end(), 0.0);
     auto h = new TH2F("confusion", ";Reco; MC", n_xbins, xbins.data(), n_ybins, ybins.data());
     h->SetStats(0);
+    
+    std::vector<Float_t> bins = {0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 20.0, 30.0, 40.0, 50.0};
+    std::cout << "bins.size(): " << bins.size() << std::endl;
+
+    auto eptstack = new THStack("eptstack", ";p_{T}");
+    // auto eept = new TH1F("eept", ";p#_T", bins.size() - 1, bins.data());
+    auto eept = new TH1F("eept", ";p_T", 25, -1, 2);
+    binLogX(eept);
+    eptstack->Add(eept);
+    // auto emupt = new TH1F("emupt", ";p#_T", bins.size() - 1, bins.data());
+    auto epipt = new TH1F("emupt", ";p_T", 25, -1, 2);
+    binLogX(epipt);
+    epipt->SetLineColor(30);
+    eptstack->Add(epipt);
+    // auto eopt = new TH1F("eopt", ";p#_T", bins.size() - 1, bins.data());
+    auto eopt = new TH1F("eopt", ";p_T", 25, -1, 2);
+    binLogX(eopt);
+    eopt->SetLineColor(9);
+    eptstack->Add(eopt);
+    
+    auto muptstack = new THStack("muptstack", ";p_{T}");
+    // auto eept = new TH1F("eept", ";p#_T", bins.size() - 1, bins.data());
+    auto mumupt = new TH1F("mumupt", ";p_T", 25, -1, 2);
+    binLogX(mumupt);
+    muptstack->Add(mumupt);
+    // auto emupt = new TH1F("emupt", ";p#_T", bins.size() - 1, bins.data());
+    auto mupipt = new TH1F("mupipt", ";p_T", 25, -1, 2);
+    binLogX(mupipt);
+    mupipt->SetLineColor(30);
+    muptstack->Add(mupipt);
+    // auto eopt = new TH1F("eopt", ";p#_T", bins.size() - 1, bins.data());
+    auto muopt = new TH1F("muopt", ";p_T", 25, -1, 2);
+    binLogX(muopt);
+    muopt->SetLineColor(9);
+    muptstack->Add(muopt);
     
     auto lcReader = LCFactory::getInstance()->createLCReader();
     lcReader->open(fileName);
@@ -66,18 +121,93 @@ void plot_SLDs(const char *fileName = "/afs/desy.de/group/flc/pool/reichenl/myMa
             // std::cout << "[mc, rec]: [" << mcPDG << ", " << recPDG << "]" << std::endl;
 
             h->Fill(rmap[recPDG], rmap[mcPDG]);
+            
+            TVector3 P(mcp->getMomentum()); 
+            
+            switch (mcPDG)
+            {
+            case 11:
+                switch (recPDG)
+                {
+                case 11:
+                    eept->Fill(P.Perp());
+                    break;
+                
+                case -211:
+                    epipt->Fill(P.Perp());
+                    break;
+                
+                case 0:
+                    eopt->Fill(P.Perp());
+                    break;
+                
+                default:
+                    break;
+                }
+                break;
+            
+            case 13:
+                switch (recPDG)
+                {
+                case 13:
+                    mumupt->Fill(P.Perp());
+                    break;
+                
+                case -211:
+                    mupipt->Fill(P.Perp());
+                    break;
+                
+                case 0:
+                    muopt->Fill(P.Perp());
+                    break;
+                
+                default:
+                    break;
+                }
+                break;
+            
+            default:
+                break;
+            }
         }
     }
     lcReader->close();
 
     gStyle->SetPalette(112);
-    auto c1 = new TCanvas("confusion plot", "foo", 800, 400);
+
+    auto c1 = new TCanvas("confc", "foo", 800, 400);
 
     // TODO: set labels
-    // TODO: make text readably big
-    // FIXME: colors not in the saved pdf..
-    // can't be fixed. It is a feature :)
-    h->Draw("COLZ2 TEXT");
+    auto xaxis = h->GetXaxis();
+    auto yaxis = h->GetYaxis();
+    xaxis->SetBinLabel(1, "11");
+    xaxis->SetBinLabel(2, "-11");
+    xaxis->SetBinLabel(3, "13");
+    xaxis->SetBinLabel(4, "-13");
+    xaxis->SetBinLabel(5, "-211");
+    xaxis->SetBinLabel(6, "211");
+    xaxis->SetBinLabel(7, "22");
+    xaxis->SetBinLabel(8, "none");
+    yaxis->SetBinLabel(1, "11");
+    yaxis->SetBinLabel(2, "-11");
+    yaxis->SetBinLabel(3, "13");
+    yaxis->SetBinLabel(4, "-13");
+    h->SetMarkerSize(4);
+    h->SetMarkerColor(0);
+    c1->cd();
+    h->Draw("COLZ TEXT");
     
     c1->SaveAs(outname + ".pdf");
+    
+    auto c2 = new TCanvas("emuhists", "");
+    c2->Divide(1, 2);
+
+    auto pad1 = c2->cd(1);
+    pad1->SetLogx();
+    eptstack->Draw();
+    auto pad2 = c2->cd(2);
+    pad2->SetLogx();
+    muptstack->Draw();
+
+    c1->SaveAs(outname + "_emu.pdf");
 }
